a807ef634abb4100238a271bd8f3651b9e343735
Tyler Norman <tnorman600@gmail.com>
2021-10-01 13:02:53 -0600

    added color coding for time of day and other small css changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
 window.setInterval(function () {
     $('#currentDay').html(moment().format('MMMM Do YYYY, h:mm:ss a'))
 }, 1000);
+
+
+function timeSatus() {
+    var hour = moment().hours();
+
+    $(".time-block").each(function() {
+        var currentHour = parseInt($(this).attr("id"));
+
+        if (currentHour > hour) {
+            $(this).addClass("future");
+        } else if (currentHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+};
+
+timeSatus();
